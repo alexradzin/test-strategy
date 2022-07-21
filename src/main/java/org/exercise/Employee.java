6f0adb713b79d22fd6e61203901bab6990d4ab2b
aradzin <32341+aradzin@users.noreply.github.corp.ebay.com>
2022-07-21 14:25:23 +0300

    fixed Employee class: now all tests pass

--- src/main/java/org/exercise/Employee.java
+++ src/main/java/org/exercise/Employee.java
@@ -1,13 +1,24 @@
 package org.exercise;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private final String firstName;
     private final String lastName;
 
     public Employee(String firstName, String lastName) {
+        this(null, firstName, lastName);
+    }
+
+    public Employee(Long id, String firstName, String lastName) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
     }
